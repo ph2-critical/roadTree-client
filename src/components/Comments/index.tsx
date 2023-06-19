e852e5602708e8e08250a5347735556ead18eab0
Yoo SeonHo <tjsgh503@naver.com>
2023-06-19 14:13:31 +0900

    feat : comment

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,24 +1,23 @@
-import Image from 'next/image';
-
 export const Comments = () => {
   return (
     <section className="py-8 bg-white dark:bg-gray-900 lg:py-16">
       <div className="max-w-2xl px-4 mx-auto">
         <div className="flex items-center justify-between mb-6">
-          <h2 className="text-lg font-bold text-gray-900 lg:text-2xl dark:text-white">
-            Discussion (20)
-          </h2>
+          <a className="text-base font-medium text-gray-900 lg:text-base dark:text-white">
+            댓글 수 3
+          </a>
         </div>
 
         <article className="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
           <footer className="flex items-center justify-between mb-2">
             <div className="flex items-center">
               <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                Michael Gough
+                강필중
               </p>
               <p className="text-sm text-gray-600 dark:text-gray-400">
                 <time dateTime="2022-02-08" title="February 8th, 2022">
                   Feb. 8, 2022
+                  {/* 현재 시간 기준으로 커스터마이징하기 */}
                 </time>
               </p>
             </div>
@@ -44,18 +43,19 @@ export const Comments = () => {
               id="dropdownComment1"
               className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600"
             >
+              {/* 더보기 버튼 눌렀을 때 여기 hidden이 풀려야됨! */}
               <ul
                 className="py-1 text-sm text-gray-700 dark:text-gray-200"
                 aria-labelledby="dropdownMenuIconHorizontalButton"
               >
-                <li>
+                {/* <li>
                   <a
                     href="#"
                     className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Edit
                   </a>
-                </li>
+                </li> */}
                 <li>
                   <a
                     href="#"
@@ -64,23 +64,18 @@ export const Comments = () => {
                     Remove
                   </a>
                 </li>
-                <li>
+                {/* <li>
                   <a
                     href="#"
                     className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                   >
                     Report
                   </a>
-                </li>
+                </li> */}
               </ul>
             </div>
           </footer>
-          <p className="text-gray-500 dark:text-gray-400">
-            Very straight-to-point article. Really worth time reading. Thank
-            you! But tools are just the instruments for the UX designers. The
-            knowledge of the design tools are as important as the creation of
-            the design strategy.
-          </p>
+          <p className="text-gray-500 dark:text-gray-400">나는 바보다</p>
           <div className="flex items-center mt-4 space-x-4">
             <button
               type="button"
@@ -101,7 +96,7 @@ export const Comments = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 ></path>
               </svg>
-              Reply
+              답글달기
             </button>
           </div>
         </article>
@@ -197,211 +192,47 @@ export const Comments = () => {
             </button>
           </div>
         </article>
-        <article className="p-6 mb-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-          <footer className="flex items-center justify-between mb-2">
-            <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                Bonnie Green
-              </p>
-              <p className="text-sm text-gray-600 dark:text-gray-400">
-                <time dateTime="2022-03-12" title="March 12th, 2022">
-                  Mar. 12, 2022
-                </time>
-              </p>
-            </div>
-            <button
-              id="dropdownComment3Button"
-              data-dropdown-toggle="dropdownComment3"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              type="button"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-              </svg>
-              <span className="sr-only">Comment settings</span>
-            </button>
-            {/* <!-- Dropdown menu --> */}
-            <div
-              id="dropdownComment3"
-              className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600"
-            >
-              <ul
-                className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownMenuIconHorizontalButton"
-              >
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Edit
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Remove
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Report
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </footer>
-          <p className="text-gray-500 dark:text-gray-400">
-            The article covers the essentials, challenges, myths and stages the
-            UX designer should consider while creating the design strategy.
-          </p>
-          <div className="flex items-center mt-4 space-x-4">
-            <button
-              type="button"
-              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-4 h-4 mr-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              Reply
-            </button>
+
+        <div className="flex">
+          <div className="flex flex-col w-1/6 gap-[4px]">
+            <span className="h-10 pt-2 text-sm border border-gray-300 rounded-lg cursor-pointer">
+              <input
+                type="text"
+                name="nickname"
+                placeholder="닉네임"
+                className="flex-grow w-11/12 px-4 text-sm rounded-lg focus:outline-none"
+              />
+            </span>
+            <span className="h-10 pt-2 text-sm border border-gray-300 rounded-lg cursor-pointer ">
+              <input
+                type="password"
+                name="password"
+                placeholder="비밀번호"
+                className="flex-grow w-11/12 px-4 text-sm rounded-lg focus:outline-none"
+              />
+            </span>
           </div>
-        </article>
-        <article className="p-6 text-base bg-white border-t border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-          <footer className="flex items-center justify-between mb-2">
-            <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                Helene Engels
-              </p>
-              <p className="text-sm text-gray-600 dark:text-gray-400">
-                <time dateTime="2022-06-23" title="June 23rd, 2022">
-                  Jun. 23, 2022
-                </time>
-              </p>
+          <form className="w-5/6 mb-6 ml-3">
+            <div className="px-4 py-2 mb-4 bg-white border border-gray-200 rounded-lg rounded-t-lg dark:bg-gray-800 dark:border-gray-700">
+              <label htmlFor="comment" className="sr-only">
+                Your comment
+              </label>
+              <textarea
+                id="comment"
+                rows={6}
+                className="w-full h-20 px-0 text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                placeholder="댓글을 입력해주세요."
+                required
+              ></textarea>
             </div>
             <button
-              id="dropdownComment4Button"
-              data-dropdown-toggle="dropdownComment4"
-              className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:bg-gray-900 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              type="button"
+              type="submit"
+              className="flex justify-center w-28 ml-auto py-2.5 px-4 text-sm font-bold text-center text-white bg-main rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
-              </svg>
+              등록
             </button>
-            {/* <!-- Dropdown menu --> */}
-            <div
-              id="dropdownComment4"
-              className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-36 dark:bg-gray-700 dark:divide-gray-600"
-            >
-              <ul
-                className="py-1 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownMenuIconHorizontalButton"
-              >
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Edit
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Remove
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >
-                    Report
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </footer>
-          <p className="text-gray-500 dark:text-gray-400">
-            Thanks for sharing this. I do came from the Backend development and
-            explored some of the tools to design my Side Projects.
-          </p>
-          <div className="flex items-center mt-4 space-x-4">
-            <button
-              type="button"
-              className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-4 h-4 mr-1"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                ></path>
-              </svg>
-              Reply
-            </button>
-          </div>
-        </article>
-        <form className="mb-6">
-          <div className="px-4 py-2 mb-4 bg-white border border-gray-200 rounded-lg rounded-t-lg dark:bg-gray-800 dark:border-gray-700">
-            <label htmlFor="comment" className="sr-only">
-              Your comment
-            </label>
-            <textarea
-              id="comment"
-              rows={6}
-              className="w-full px-0 text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-              placeholder="Write a comment..."
-              required
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-          >
-            Post comment
-          </button>
-        </form>
+          </form>
+        </div>
       </div>
     </section>
   );
