6511844e64ba5a70aba0a3e06c345d7b9c324177
CodeBlue0 <piri007@cau.ac.kr>
2023-07-11 16:57:17 +0900

    chore: roadmap 구조 변경

--- src/components/RoadmapPage/RoadTreeLayout.tsx
+++ src/components/RoadmapPage/RoadTreeLayout.tsx
@@ -8,7 +8,7 @@ import {
 } from '@/roadmap_json/roadmap_data';
 import { getDatas, getProps } from '@/src/api';
 import * as d3 from 'd3';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { create } from 'zustand';
 
 interface RoadTreeStore {
@@ -31,6 +31,7 @@ export interface RoadTreeLayOutProps {
 }
 
 export default function RoadTreeLayout(props: RoadTreeLayOutProps) {
+  const [init, setInit] = useState<boolean>(false);
   const { setSelect, setUpdateFunc } = useRoadTreeStore();
   const selecthistory: (null | RoadData)[] = [null, null, null];
   let selecthistorybefore: (null | RoadData)[] = [null, null, null]; // 이전에 선택된 내용. 이 내용을 토대로 노드가 사라짐
@@ -48,299 +49,295 @@ export default function RoadTreeLayout(props: RoadTreeLayOutProps) {
   };
 
   useEffect(() => {
-    console.log('user id :' + userId);
-    const getDataRequest: getProps = {
-      db: 'front_node_depth2',
-      user_id: userId,
-    };
-    const getData = async () => {
-      const data = await getDatas(getDataRequest);
-      console.log(data);
-    };
-    getData();
+    if (userId && init === false) {
+      console.log('user id :' + userId);
+      setInit(true);
+    }
   }, [userId]);
 
   useEffect(() => {
-    let m = [20, 120, 20, 20],
-      w = 1280 - m[1] - m[3],
-      h = 800 - m[0] - m[2],
-      i = 0;
-    const root: RoadData =
-      whatStudy == 0 ? roadmap_front_public : roadmap_back_public;
-    const tree: any = d3.layout.tree().size([h, w]);
+    if (init) {
+      let m = [20, 120, 20, 20],
+        w = 1280 - m[1] - m[3],
+        h = 800 - m[0] - m[2],
+        i = 0;
+      const root: RoadData =
+        whatStudy == 0 ? roadmap_front_public : roadmap_back_public;
+      const tree: any = d3.layout.tree().size([h, w]);
 
-    const diagonal = d3.svg.diagonal().projection(function (d: RoadData) {
-      return [d.y, d.x];
-    });
-
-    const vis = d3
-      .select('#body')
-      .append('svg:svg')
-      .attr('width', w + m[1] + m[3])
-      .attr('height', h + m[0] + m[2])
-      .append('svg:g')
-      .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
-
-    root.x0 = h / 2;
-    root.y0 = 0;
-    setSelect(null);
-    if (root.children instanceof Array) {
-      root.children.map((child: RoadData) => {
-        if (child.children !== undefined && child.children !== null) {
-          child.children.map((child2) => {
-            toggle_deleteselect(child2);
-          });
-          toggle_deleteselect(child);
-        }
-        child.select = false;
-      });
-    }
-
-    update(root);
-
-    function update(source: RoadData) {
-      let duration = d3.event && d3.event.altKey ? 5000 : 500;
-
-      // Compute the new tree layout.
-      let nodes = tree.nodes(root).reverse();
-
-      // Normalize for fixed-depth.
-      nodes.forEach(function (d: RoadData) {
-        let level: number = getLevel();
-        if (d.depth) {
-          d.y = (d.depth - level / 3) * 300 + 100;
-        } else {
-          d.y = (0 - level / 3) * 300 + 300;
-        }
+      const diagonal = d3.svg.diagonal().projection(function (d: RoadData) {
+        return [d.y, d.x];
       });
 
-      // Update the nodes…
-      let node = vis.selectAll('g.node').data(nodes, function (d: RoadData) {
-        return d.id || (d.id = ++i);
-      });
-
-      // Enter any new nodes at the parent's previous position.
-      let nodeEnter = node
-        .enter()
+      const vis = d3
+        .select('#body')
+        .append('svg:svg')
+        .attr('width', w + m[1] + m[3])
+        .attr('height', h + m[0] + m[2])
         .append('svg:g')
-        .attr('class', function (d: RoadData) {
-          return 'node' + (d.depth === 0 ? ' hidden ' : '');
-        })
-        .attr('transform', function () {
-          return 'translate(' + source.y0 + ',' + source.x0 + ')';
-        })
-        .on('click', function (d: RoadData) {
-          toggle_select(d);
-          lastclick = d;
-          update(d);
-        });
-      nodeEnter
-        .append('svg:rect')
-        .attr('class', 'fill-white')
-        .style('fill', '#fff')
-        .style('width', '200')
-        .style('height', '40')
-        .style('x', '-100')
-        .style('y', '-20')
-        .style('rx', '10')
-        .style('ry', '10');
-      nodeEnter = nodeEnter
-        .append('svg:g')
-        .attr('class', function (d: RoadData) {
-          return 'cursor-pointer hover:brightness-95 hover:opacity-100 ';
-        });
+        .attr('transform', 'translate(' + m[3] + ',' + m[0] + ')');
 
-      nodeEnter
-        .append('svg:rect')
-        .attr('class', 'fill-white stroke-black stroke-2 cursor-pointer')
-        .style('fill', '#fff')
-        .style('width', '200')
-        .style('height', '40')
-        .style('x', '-100')
-        .style('y', '-20')
-        .style('rx', '10')
-        .style('ry', '10');
-
-      nodeEnter
-        .append('svg:text')
-        .attr('x', 0)
-        .attr('y', 0)
-        .attr('text-anchor', 'middle')
-        .text(function (d: RoadData) {
-          return d.name;
-        })
-        .attr('alignment-baseline', 'middle')
-        .attr('font-weight', 'bold');
-
-      // Transition nodes to their new position.
-      node
-        .transition()
-        .duration(duration)
-        .attr('transform', function (d: RoadData) {
-          return 'translate(' + d.y + ',' + d.x + ')';
-        });
-      let nodeUpdate = node.select('g').attr('class', function (d: RoadData) {
-        return (
-          'cursor-pointer hover:brightness-95 hover:opacity-100 ' +
-          (d.depth === 0 ? ' hidden ' : '') +
-          (d.select
-            ? 'brightness-90 '
-            : selectcurrent !== null &&
-              selectcurrent.select === true &&
-              d !== selecthistory[d.depth! - 1] &&
-              getLevel() >= (d.depth === undefined ? 0 : d.depth)
-            ? 'opacity-30 '
-            : '')
-        );
-      });
-
-      nodeUpdate
-        .select('rect')
-        .attr('class', function (d: RoadData) {
-          return 'stroke-black stroke-2  ';
-        })
-        .style('width', '200')
-        .style('height', '40')
-        .style('x', '-100')
-        .style('y', '-20')
-        .style('rx', '10')
-        .style('ry', '10')
-        .style('fill', function (d: RoadData) {
-          return statebgColor[d.state ?? 0];
-        });
-
-      // Transition exiting nodes to the parent's new position.
-      let nodeExit = node
-        .exit()
-        .transition()
-        .duration(function (d: RoadData) {
-          if ((d.depth ?? 1) - (lastclick!.depth ?? 1) >= 2) return 0;
-          else return duration;
-        })
-        .attr('transform', function (d: RoadData) {
-          return (
-            'translate(' +
-            selecthistorybefore[(d!.depth ?? 2) - 2]!.y +
-            ',' +
-            selecthistorybefore[(d!.depth ?? 2) - 2]!.x +
-            ')'
-          );
-        })
-        .remove();
-
-      // Update the links…
-      let link = vis
-        .selectAll('path.link')
-        .data(tree.links(nodes), function (d: any) {
-          return d.target.id;
-        });
-
-      // Enter any new links at the parent's previous position.
-      link
-        .enter()
-        .insert('svg:path', 'g')
-        .attr('class', function (d: { source: RoadData; target: RoadData }) {
-          return (
-            'link fill-none stroke-black stroke-1' +
-            (d.source.depth === 0 ? ' hidden' : '')
-          );
-        })
-        .attr('d', function () {
-          let o = { x: source.x0, y: source.y0 };
-          return diagonal({ source: o, target: o });
-        })
-        .transition()
-        .duration(duration)
-        .attr('d', diagonal);
-
-      // Transition links to their new position.
-      link.transition().duration(duration).attr('d', diagonal);
-
-      //   Transition exiting nodes to the parent's new position.
-      link
-        .exit()
-        .transition()
-        .duration(function (d: { source: RoadData; target: RoadData }) {
-          if ((d.source.depth ?? 1) - (lastclick!.depth ?? 1) >= 1) return 0;
-          else return duration;
-        })
-        .attr('d', function (d: { source: RoadData; target: RoadData }) {
-          let o = {
-            x: selecthistorybefore[(d.source.depth ?? 1) - 1]!.x,
-            y: selecthistorybefore[(d.source.depth ?? 1) - 1]!.y,
-          };
-          return diagonal({ source: o, target: o });
-        })
-        .remove();
-
-      // Stash the old positions for transition.
-      nodes.forEach(function (d: RoadData) {
-        d.x0 = d.x;
-        d.y0 = d.y;
-      });
-    }
-
-    // 선택
-    function toggle_select(d: RoadData) {
-      if (d.select === true) {
-        for (let i = 3; i >= (d.depth ?? 1) - 1; i--) {
-          if (selecthistory[i] !== null) {
-            toggle_deleteselect(selecthistory[i]!);
+      root.x0 = h / 2;
+      root.y0 = 0;
+      setSelect(null);
+      if (root.children instanceof Array) {
+        root.children.map((child: RoadData) => {
+          if (child.children !== undefined && child.children !== null) {
+            child.children.map((child2) => {
+              toggle_deleteselect(child2);
+            });
+            toggle_deleteselect(child);
           }
-        }
-        return;
+          child.select = false;
+        });
       }
 
-      if (d._children !== null) {
-        // 선택
-        d.children = d._children;
-        d._children = null;
+      update(root);
+
+      function update(source: RoadData) {
+        let duration = d3.event && d3.event.altKey ? 5000 : 500;
+
+        // Compute the new tree layout.
+        let nodes = tree.nodes(root).reverse();
+
+        // Normalize for fixed-depth.
+        nodes.forEach(function (d: RoadData) {
+          let level: number = getLevel();
+          if (d.depth) {
+            d.y = (d.depth - level / 3) * 300 + 100;
+          } else {
+            d.y = (0 - level / 3) * 300 + 300;
+          }
+        });
+
+        // Update the nodes…
+        let node = vis.selectAll('g.node').data(nodes, function (d: RoadData) {
+          return d.id || (d.id = ++i);
+        });
+
+        // Enter any new nodes at the parent's previous position.
+        let nodeEnter = node
+          .enter()
+          .append('svg:g')
+          .attr('class', function (d: RoadData) {
+            return 'node' + (d.depth === 0 ? ' hidden ' : '');
+          })
+          .attr('transform', function () {
+            return 'translate(' + source.y0 + ',' + source.x0 + ')';
+          })
+          .on('click', function (d: RoadData) {
+            toggle_select(d);
+            lastclick = d;
+            update(d);
+          });
+        nodeEnter
+          .append('svg:rect')
+          .attr('class', 'fill-white')
+          .style('fill', '#fff')
+          .style('width', '200')
+          .style('height', '40')
+          .style('x', '-100')
+          .style('y', '-20')
+          .style('rx', '10')
+          .style('ry', '10');
+        nodeEnter = nodeEnter
+          .append('svg:g')
+          .attr('class', function (d: RoadData) {
+            return 'cursor-pointer hover:brightness-95 hover:opacity-100 ';
+          });
+
+        nodeEnter
+          .append('svg:rect')
+          .attr('class', 'fill-white stroke-black stroke-2 cursor-pointer')
+          .style('fill', '#fff')
+          .style('width', '200')
+          .style('height', '40')
+          .style('x', '-100')
+          .style('y', '-20')
+          .style('rx', '10')
+          .style('ry', '10');
+
+        nodeEnter
+          .append('svg:text')
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('text-anchor', 'middle')
+          .text(function (d: RoadData) {
+            return d.name;
+          })
+          .attr('alignment-baseline', 'middle')
+          .attr('font-weight', 'bold');
+
+        // Transition nodes to their new position.
+        node
+          .transition()
+          .duration(duration)
+          .attr('transform', function (d: RoadData) {
+            return 'translate(' + d.y + ',' + d.x + ')';
+          });
+        let nodeUpdate = node.select('g').attr('class', function (d: RoadData) {
+          return (
+            'cursor-pointer hover:brightness-95 hover:opacity-100 ' +
+            (d.depth === 0 ? ' hidden ' : '') +
+            (d.select
+              ? 'brightness-90 '
+              : selectcurrent !== null &&
+                selectcurrent.select === true &&
+                d !== selecthistory[d.depth! - 1] &&
+                getLevel() >= (d.depth === undefined ? 0 : d.depth)
+              ? 'opacity-30 '
+              : '')
+          );
+        });
+
+        nodeUpdate
+          .select('rect')
+          .attr('class', function (d: RoadData) {
+            return 'stroke-black stroke-2  ';
+          })
+          .style('width', '200')
+          .style('height', '40')
+          .style('x', '-100')
+          .style('y', '-20')
+          .style('rx', '10')
+          .style('ry', '10')
+          .style('fill', function (d: RoadData) {
+            return statebgColor[d.state ?? 0];
+          });
+
+        // Transition exiting nodes to the parent's new position.
+        let nodeExit = node
+          .exit()
+          .transition()
+          .duration(function (d: RoadData) {
+            if ((d.depth ?? 1) - (lastclick!.depth ?? 1) >= 2) return 0;
+            else return duration;
+          })
+          .attr('transform', function (d: RoadData) {
+            return (
+              'translate(' +
+              selecthistorybefore[(d!.depth ?? 2) - 2]!.y +
+              ',' +
+              selecthistorybefore[(d!.depth ?? 2) - 2]!.x +
+              ')'
+            );
+          })
+          .remove();
+
+        // Update the links…
+        let link = vis
+          .selectAll('path.link')
+          .data(tree.links(nodes), function (d: any) {
+            return d.target.id;
+          });
+
+        // Enter any new links at the parent's previous position.
+        link
+          .enter()
+          .insert('svg:path', 'g')
+          .attr('class', function (d: { source: RoadData; target: RoadData }) {
+            return (
+              'link fill-none stroke-black stroke-1' +
+              (d.source.depth === 0 ? ' hidden' : '')
+            );
+          })
+          .attr('d', function () {
+            let o = { x: source.x0, y: source.y0 };
+            return diagonal({ source: o, target: o });
+          })
+          .transition()
+          .duration(duration)
+          .attr('d', diagonal);
+
+        // Transition links to their new position.
+        link.transition().duration(duration).attr('d', diagonal);
+
+        //   Transition exiting nodes to the parent's new position.
+        link
+          .exit()
+          .transition()
+          .duration(function (d: { source: RoadData; target: RoadData }) {
+            if ((d.source.depth ?? 1) - (lastclick!.depth ?? 1) >= 1) return 0;
+            else return duration;
+          })
+          .attr('d', function (d: { source: RoadData; target: RoadData }) {
+            let o = {
+              x: selecthistorybefore[(d.source.depth ?? 1) - 1]!.x,
+              y: selecthistorybefore[(d.source.depth ?? 1) - 1]!.y,
+            };
+            return diagonal({ source: o, target: o });
+          })
+          .remove();
+
+        // Stash the old positions for transition.
+        nodes.forEach(function (d: RoadData) {
+          d.x0 = d.x;
+          d.y0 = d.y;
+        });
       }
 
-      if (d.depth) {
-        if (
-          selecthistory[d.depth - 1] !== null &&
-          selecthistory[d.depth - 1] !== d
-        ) {
-          for (let i = 3; i >= d.depth - 1; i--) {
+      // 선택
+      function toggle_select(d: RoadData) {
+        if (d.select === true) {
+          for (let i = 3; i >= (d.depth ?? 1) - 1; i--) {
             if (selecthistory[i] !== null) {
               toggle_deleteselect(selecthistory[i]!);
             }
           }
+          return;
         }
-        selecthistory[d.depth - 1] = d;
-      }
 
-      if (selectcurrent !== null) selectcurrent.select = false; // 이전 선택 내용 색 지우기
-      d.select = true; // 선택된 내용 색 넣기
-      setSelect(d);
-      selectcurrent = d; // 이전 선택 내용 업데이트
-    }
+        if (d._children !== null) {
+          // 선택
+          d.children = d._children;
+          d._children = null;
+        }
 
-    // 선택해제
-    function toggle_deleteselect(d: RoadData) {
-      if (d) {
-        if (d.children) {
-          // 선택 해제
-          d._children = d.children;
-          d.children = null;
-          if (d.depth) {
-            selecthistorybefore[d.depth - 1] = selecthistory[d.depth - 1];
-            selecthistory[d.depth - 1] = null;
+        if (d.depth) {
+          if (
+            selecthistory[d.depth - 1] !== null &&
+            selecthistory[d.depth - 1] !== d
+          ) {
+            for (let i = 3; i >= d.depth - 1; i--) {
+              if (selecthistory[i] !== null) {
+                toggle_deleteselect(selecthistory[i]!);
+              }
+            }
           }
+          selecthistory[d.depth - 1] = d;
         }
 
-        if (selectcurrent !== null) selectcurrent.select = false;
-        d.select = false;
-        setSelect(null);
-        selectcurrent = null;
+        if (selectcurrent !== null) selectcurrent.select = false; // 이전 선택 내용 색 지우기
+        d.select = true; // 선택된 내용 색 넣기
+        setSelect(d);
+        selectcurrent = d; // 이전 선택 내용 업데이트
       }
-    }
 
-    setUpdateFunc((select: RoadData) => {
-      update(select);
-    });
-  }, []);
+      // 선택해제
+      function toggle_deleteselect(d: RoadData) {
+        if (d) {
+          if (d.children) {
+            // 선택 해제
+            d._children = d.children;
+            d.children = null;
+            if (d.depth) {
+              selecthistorybefore[d.depth - 1] = selecthistory[d.depth - 1];
+              selecthistory[d.depth - 1] = null;
+            }
+          }
+
+          if (selectcurrent !== null) selectcurrent.select = false;
+          d.select = false;
+          setSelect(null);
+          selectcurrent = null;
+        }
+      }
+
+      setUpdateFunc((select: RoadData) => {
+        update(select);
+      });
+    }
+  }, [init]);
 
   return <div id="body" className="w-auto overflow-scroll scrollbar-hide" />;
 }
