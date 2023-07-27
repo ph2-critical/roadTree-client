14a79a04a09a56f140797b9bcb9e1d448dcfbe4f
CodeBlue0 <piri007@cau.ac.kr>
2023-07-27 13:17:25 +0900

    feat: alarm 반응형

--- src/components/Alarm/index.tsx
+++ src/components/Alarm/index.tsx
@@ -16,23 +16,23 @@ export const Alarm = () => {
                     alt={'notification'}
                     width={512}
                     height={512}
-                    className="w-5 h-5" 
+                    className="w-5 h-5"
                 ></Image>
             </button>
 
-             {myPageIsOpen && (
-                <div className="sm:absolute z-10 text-black bg-white sm:w-96 sm:mt-5  sm:right-[-30px] sm:shadow-deep-dark sm:rounded-md sm:p-2
-                w-screen">
-                    {[1,2,3].map((item, index) => {
+            {myPageIsOpen && (
+                <div className="z-10 text-black bg-white sm:absolute sm:w-96 sm:h-auto sm:mt-5 sm:top-auto sm:right-[-30px] sm:shadow-deep-dark sm:rounded-md
+                fixed w-full right-0 top-[72px] h-screenWithoutHeader px-2">
+                    {[1, 2, 3].map((item, index) => {
                         return (
-                            <div className="flex items-center justify-between rounded-md shadow-deep-dark p-3 mb-2 hover:bg-gray-100 cursor-pointer">
+                            <div className="flex items-center justify-between rounded-md shadow-deep-dark p-3 my-2 hover:bg-gray-100 cursor-pointer">
                                 <div className="flex items-center">
                                     <div className="w-10 h-10 mr-3 bg-gray-200 rounded-full"></div>
                                     <div className="flex flex-col">
                                         <div className="text-sm">알림이 왔습니다.</div>
                                     </div>
                                 </div>
-                                
+
                             </div>
                         )
                     })}
