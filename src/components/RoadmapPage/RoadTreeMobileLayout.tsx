25b81f3ffac86009f8278e0a549071a3ac7dc9ee
CodeBlue0 <piri007@cau.ac.kr>
2023-07-31 20:46:44 +0900

    feat: roadtreemobile menu 화살표 추가

--- src/components/RoadmapPage/RoadTreeMobileLayout.tsx
+++ src/components/RoadmapPage/RoadTreeMobileLayout.tsx
@@ -1,5 +1,6 @@
 import { RoadData } from "@/roadmap_json/roadmap_data";
 import { useRoadTreeStore } from "./RoadTreeLayout";
+import Image from "next/image";
 
 export default function RoadTreeMobileLayout(props: {
     roadData: RoadData;
@@ -15,9 +16,20 @@ export default function RoadTreeMobileLayout(props: {
             return (
                 data.children.map((child, idx) => {
                     return (
-                        <div>
-                            <div className={`border-2 border-black w-full rounded-lg h-20 mt-4 mx-1 p-4`}
-                                onClick={() => { toggleSelect(child); updateFunc(child) }}>{child.name}</div>
+                        <div className="px-2 pb-2">
+                            <div className={`border-2 border-black w-full rounded-lg h-20 mt-4 p-4 hover:brightness-95 bg-white cursor-pointer
+                                ${child.select ? ' brightness-90 ' : ''} flex flex-row items-center
+                                text-lg font-bold text-gray-700 pl-8`}
+                                onClick={() => { toggleSelect(child); updateFunc(child) }}>
+                                {child.name}
+
+                                <Image
+                                    src='/roadTreeMobile/downArrow.svg'
+                                    alt='downArrow'
+                                    width={50}
+                                    height={50}
+                                    className={`rounded-full ml-auto transition-transform ${child.select ? 'rotate-180' : ''}`} />
+                            </div>
                             {renderChildren(child)}
                         </div>
                     )
