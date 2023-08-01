ddf5118ac8496c7c2099dbd179bcd38ed7b4dd8d
CodeBlue0 <piri007@cau.ac.kr>
2023-08-01 20:59:13 +0900

    refactor: useWindowResize 파일 변경

--- src/utils/hooks/useWindowResize.tsx
+++ src/utils/hooks/useWindowResize.tsx
@@ -20,6 +20,26 @@ export const useWindowResize = () => {
   return isMobile;
 };
 
+export const usemdResize = () => {
+  const [ismdSize, setIsmdSize] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsmdSize(window.matchMedia("(max-width: 428px)").matches);
+    };
+
+    handleResize(); // 초기 렌더링 시 크기 확인
+
+    window.addEventListener("resize", handleResize); // resize 이벤트 감지
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // 컴포넌트 언마운트 시 이벤트 제거
+    };
+  }, []);
+
+  return ismdSize;
+};
+
 export const useWindowTabletResize = () => {
   const [isTablet, setIsTablet] = useState(false);
 
