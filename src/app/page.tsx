b867c4ab6134594aa4f230151b8914ab6394215f
Yoo SeonHo <tjsgh503@naver.com>
2023-09-01 15:35:45 +0900

    feat : 랜딩 페이지 1차 완성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { track } from "@amplitude/analytics-browser";
 
 import { useEffect } from "react";
+import { Logo } from "../assets/Icons";
 
 export default function Home() {
   useEffect(() => {
@@ -35,7 +36,7 @@ export default function Home() {
     // </main>
     <main>
       <section className="bg-[#FEFFFE] dark:bg-gray-900">
-        <div className="max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16 lg:px-12">
+        <div className="max-w-screen-xl px-4 pt-8 mx-auto text-center lg:pt-16 lg:px-12">
           <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
             프로그래밍의 <br className="sm:hidden" /> A부터 Z까지
           </h1>
@@ -86,22 +87,22 @@ export default function Home() {
       </section>
 
       <section className="bg-[#FBFFFB] dark:bg-gray-900">
-        <div className="max-w-screen-xl px-4 py-8 mx-auto space-y-12 lg:space-y-20 lg:px-6">
+        <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-12 text-center lg:text-start lg:space-y-20 lg:px-6">
           {/* <!-- Row --> */}
           <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h1 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
                 입문자를 위한
                 <div className="mt-3" />
                 직관적인 로드맵
-              </h2>
+              </h1>
               <p className="mb-8 font-light lg:text-xl">
                 개발자가 되고 싶은 당신을 위한 로드맵을 제공합니다. <br />
                 난이도 별 학습을 통해 로드트리와 함께 당신의 목표를 달성하세요.
               </p>
             </div>
             <img
-              className="hidden w-7/12 mb-4 rounded-lg lg:mb-0 lg:flex"
+              className="hidden w-2/3 mb-4 rounded-lg lg:mb-0 lg:flex"
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/features/feature-office-1.png"
               alt="office feature image"
             />
@@ -109,8 +110,10 @@ export default function Home() {
           {/* <!-- Row --> */}
           <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <img
-              className="hidden w-7/12 mb-4 rounded-lg lg:mb-0 lg:flex"
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/features/feature-office-2.png"
+              className="hidden w-11/12 mb-4 rounded-lg lg:mb-0 lg:flex"
+              src="/landing/drag.svg"
+              width={500}
+              height={300}
               alt="office feature image 2"
             />
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
@@ -126,24 +129,62 @@ export default function Home() {
           {/* <!-- Row --> */}
           <div className="items-center gap-8 lg:grid lg:grid-cols-2 xl:gap-16">
             <div className="text-gray-500 sm:text-lg dark:text-gray-400">
-              <h2 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h3 className="mb-4 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">
                 학습 기록을 통한
                 <div className="mt-3" />
                 완벽한 성장
-              </h2>
+              </h3>
               <p className="mb-8 font-light lg:text-xl">
                 간단히 학습한 내용을 기록해보세요. <br />
                 하나하나가 모여 당신의 성장을 만들어갑니다.
               </p>
             </div>
             <img
-              className="hidden w-7/12 mb-4 rounded-lg lg:mb-0 lg:flex"
+              className="hidden w-2/3 mb-4 rounded-lg lg:mb-0 lg:flex"
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/features/feature-office-1.png"
               alt="office feature image"
             />
           </div>
         </div>
       </section>
+
+      <footer className="p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
+        <div className="max-w-screen-xl mx-auto text-center">
+          <a
+            href="/"
+            className="flex items-center justify-center text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <Logo />
+          </a>
+          <div className="flex flex-col mt-5 mb-8 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            <a
+              href="/roadmap"
+              className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white rounded-lg bg-main hover:bg-green-800 focus:ring-4 focus:ring-todoColor dark:focus:ring-green-900"
+            >
+              로드트리 시작하기
+              <svg
+                className="w-5 h-5 ml-2 -mr-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
+          </div>
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="/" className="hover:underline">
+              Nobase™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
     </main>
   );
 }
