6e25b5d3ba5c0cef874934d7a2b1fbdd1185c10c
Yoo SeonHo <tjsgh503@naver.com>
2023-09-13 21:52:03 +0900

    feat : 상세페이지 UI 완성

--- src/app/(contents)/signup/page.tsx
+++ src/app/(contents)/signup/page.tsx
@@ -1,20 +1,43 @@
 "use client";
 
+import { useState } from "react";
+import { useNicknameStore } from "@/src/state/store";
+import Image from "next/image";
+
+interface PrivacyInterface {
+  nickname: string;
+  email: string;
+  age: number;
+  gender: string;
+  status: string;
+  category: string;
+}
+
 export default function Page() {
+  const { nickname, email, setNickname, userPicture } = useNicknameStore();
+  const [privacy, setPrivacy] = useState<PrivacyInterface>({
+    nickname: nickname,
+    email: email,
+    age: 0,
+    gender: "",
+    status: "",
+    category: "",
+  });
+
   return (
     <section className="flex h-[calc(100vh-72px)]">
       <form className="container max-w-2xl m-auto shadow-md md:w-3/4">
-        <div className="p-4 border-t-2 border-indigo-400 rounded-lg bg-gray-100/5 ">
+        <div className="p-4 border-t-2 rounded-lg border-doneColor bg-gray-100/5 ">
           <div className="max-w-sm mx-auto md:w-full md:mx-0">
             <div className="inline-flex items-center space-x-4">
-              <a href="#" className="relative block">
-                <img
-                  alt="profil"
-                  src="/images/person/1.jpg"
-                  className="object-cover w-16 h-16 mx-auto rounded-full "
-                />
-              </a>
-              <h1 className="text-gray-600">Charlie</h1>
+              <Image
+                alt="profile"
+                src={userPicture ?? "/header/user.svg"}
+                width={512}
+                height={512}
+                className="object-cover w-16 h-16 mx-auto rounded-full "
+              />
+              <h1 className="text-gray-600">{nickname}</h1>
             </div>
           </div>
         </div>
@@ -27,8 +50,10 @@ export default function Page() {
                 <input
                   type="text"
                   id="user-info-email"
-                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                  className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                   placeholder="Email"
+                  value={email}
+                  disabled
                 />
               </div>
             </div>
@@ -42,22 +67,16 @@ export default function Page() {
                   <input
                     type="text"
                     id="user-info-age"
-                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-transparent border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                    className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg shadow-sm appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                     placeholder="나이"
                   />
                 </div>
               </div>
               <div className="flex w-full gap-4 pb-4 text-gray-500 ">
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                >
+                <button className="signup-btn bg-[#13D080]" type="button">
                   남성
                 </button>
-                <button
-                  type="button"
-                  className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                >
+                <button className="signup-btn bg-[#13D080]" type="button">
                   여성
                 </button>
               </div>
@@ -70,22 +89,13 @@ export default function Page() {
             <div className="max-w-sm mx-auto space-y-5 md:w-2/3">
               <div>
                 <div className="flex w-full gap-4 pb-4 text-gray-500 ">
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                  >
+                  <button type="button" className="signup-btn bg-[#13D080]">
                     전공자
                   </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                  >
+                  <button type="button" className="signup-btn bg-[#13D080]">
                     비전공자
                   </button>
-                  <button
-                    type="button"
-                    className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-                  >
+                  <button type="button" className="signup-btn bg-[#13D080]">
                     현업자
                   </button>
                 </div>
@@ -101,15 +111,13 @@ export default function Page() {
                   <option value="2">백엔드</option>
                   <option value="3">앱 개발</option>
                   <option value="4">AI</option>
+                  <option value="5">기타</option>
                 </select>
               </div>
             </div>
           </div>
           <div className="w-full px-4 pb-4 ml-auto text-gray-500 md:w-1/3">
-            <button
-              type="submit"
-              className="w-full px-4 py-2 text-base font-semibold text-center text-white transition duration-200 ease-in bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 "
-            >
+            <button type="submit" className="signup-btn bg-[#13D080]">
               Save
             </button>
           </div>
