e6a66a34421da0946229ac605f5d281b9f99397b
Yoo SeonHo <tjsgh503@naver.com>
2023-10-20 23:57:19 +0900

    fix : type error

--- src/app/(noLayout)/edit/page.tsx
+++ src/app/(noLayout)/edit/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 import { getUserInfo } from "@/src/api/signup";
-import EditProfile, { ProfileProps } from "@/src/components/Profile";
+import EditProfile from "@/src/components/Profile";
 import { useLoginStore } from "@/src/state/store";
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
-interface EditInterface extends ProfileProps {
-  profile_image: string;
+interface UserInfo {
+  created_at: string;
+  email: string;
+  id: string;
+  nickname: string;
+  gender: string | null;
+  age: number | null;
+  career: string | null;
+  stack: string | null;
+  profile_image: string | null;
 }
 
 export default function Page() {
   const { userId } = useLoginStore();
-  const [data, setData] = useState<EditInterface>();
+  const [data, setData] = useState<UserInfo>();
   const path = usePathname();
   useEffect(() => {
     getUserInfo(userId).then((res) => {
