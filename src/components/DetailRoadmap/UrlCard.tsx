920e1914bfb97f706a2a883c29a9e126bdf59b28
CodeBlue0 <piri007@cau.ac.kr>
2023-10-22 11:23:55 +0900

    fix/default-image 추가

--- src/components/DetailRoadmap/UrlCard.tsx
+++ src/components/DetailRoadmap/UrlCard.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 
 interface ogData {
     ogData: {
-        ogTitle: string;
+        ogTitle?: string;
         ogImage: {
             url: string;
         }[]
@@ -23,23 +23,34 @@ export function UrlCard(props: { rid: string, url: string }) {
 
     useEffect(() => {
         try {
-            fetch("/api/getog/" + props.rid).then((res) => res.json()).then((data) => { setOgInfo(data); });
+            fetch("/api/getog/" + props.rid).then((res) => res.json()).then((data) => {
+                if (data.ogData === undefined) {
+                    data.ogData = {
+                        ogTitle: "제목 없음",
+                        ogDescription: "설명 없음",
+                        ogImage: [{ url: "/detailRef/default-image.jpg" }]
+                    }
+                }
+                else if (data.ogData.ogImage === undefined) {
+                    data.ogData.ogImage = [{ url: "/detailRef/default-image.jpg" }];
+                }
+                setOgInfo(data);
+            });
         } catch (error) {
 
         }
-
     }, []);
     return (
         ogInfo === null ? <svg className="animate-spin h-5 w-5 mr-3 bg-main" viewBox="0 0 24 24"></svg> :
             <Link href={url ?? '#'} target="_blank">
                 <div className="bg-slate-50 shadow-lg border">
                     <img
-                        src={ogInfo?.ogData.ogImage[0].url ?? "http://tcpschool.com/img/tcpschool_og_logo.png"}
-                        alt={ogInfo?.ogData.ogTitle ?? "잘못된 url"}
+                        src={ogInfo?.ogData?.ogImage[0]?.url ?? "/detailRef/default-image.jpg"}
+                        alt={url ? (ogInfo?.ogData?.ogTitle ?? "제목 없음") : "잘못된 url"}
                         className="w-64 h-36 object-center object-cover"
                     />
-                    <div className="px-4 pt-2 text-sm font-medium break-all w-64">{ogInfo?.ogData.ogTitle}</div>
-                    <div className="px-4 font-normal text-sm text-gray-500 break-all w-64 line-clamp-3 hover:line-clamp-none">{ogInfo?.ogData.ogDescription}</div>
+                    <div className="px-4 pt-2 text-sm font-medium break-all w-64">{ogInfo?.ogData?.ogTitle ?? "제목 없음"}</div>
+                    <div className="px-4 font-normal text-sm text-gray-500 break-all w-64 line-clamp-3 hover:line-clamp-none">{ogInfo?.ogData?.ogDescription}</div>
                     <div className="px-4 font-light text-xs text-gray-300 break-all w-64">{url}</div>
                 </div>
             </Link >
