350677ead92d798e66779ffba94c3b25e8a1105f
CodeBlue0 <piri007@cau.ac.kr>
2023-11-17 14:43:44 +0900

    chore: 프로필 이미지 github 허용

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     protocol: "https",
-    domains: ["lh3.googleusercontent.com", "k.kakaocdn.net"],
+    domains: ["lh3.googleusercontent.com", "k.kakaocdn.net, github.com"],
   },
   eslint: {
     ignoreDuringBuilds: true,
